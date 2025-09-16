dcb46ce618e9563d6d963547c2ce4889ea61dff3
Yokub2144 <Suwat.Paomong@gmail.com>
2025-09-17 00:57:07 +0700

    Update home page to display CreditPages instead of LoadPages

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter Demo', home: LoadPages());
+    return const MaterialApp(title: 'Flutter Demo', home: CreditPages());
   }
 }
